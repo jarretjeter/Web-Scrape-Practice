534227a4a3afd959a2bb6002fdbb307e2ef44392
Jarret Jeter <jarretjeter@yahoo.com>
2022-07-04 23:45:46 -0700

    add code to retrieve all links on a reddit webpage

--- nestle_scrape.py
+++ nestle_scrape.py
@@ -1,3 +1,21 @@
+from pprint import pprint
 from bs4 import BeautifulSoup
 import requests
 
+result = requests.get("https://www.reddit.com/r/news/comments/mzp1hg/droughthit_california_orders_nestl%C3%A9_to_stop/")
+
+# print(result.status_code)
+# print(result.headers)
+# pprint(result.headers)
+
+src = result.content
+print(src)
+
+soup = BeautifulSoup(src, features="html.parser")
+links = soup.find_all("a")
+# print(f"LINKS: \n{links}")
+# print("\n")
+
+for link in links:
+    print(f"LINK: {link}")
+    print(f"HREF: {link.attrs['href']}")
